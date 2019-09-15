8933305a6d9b467fe0e20941e57b20f52454df17
iw00t <iw00t@protonmail.com>
2019-09-15 00:02:38 +0000

    Added Google Test framework.

---
+++ tests/tests.cpp
@@ -0,0 +1,8 @@
+#include "../include/gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    // The following line must be executed to initialize Google Mock
+    // (and Google Test) before running the tests.
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
