4c5f32808a56ce02bdb7a49436c5d8efb8c0d0ad
iw00t <iw00t@protonmail.com>
2019-10-05 00:38:15 +0000

    Obtain login cookies for authentication with API.   * APIModel now stores a pointer to a cpr::Session and is able to retrieve     login cookies for subsequent API authentication.   * Updated .gitignore to include bin folder.

--- src/model/APIModel.cpp
+++ src/model/APIModel.cpp
@@ -1,6 +1,8 @@
 #include "APIModel.h"
 
-#include <cpr/cpr.h>
+#include <map>
+#include <iostream>
+#include <fstream>
 
 namespace REDBetterR {
     namespace API {
@@ -16,18 +18,38 @@ namespace REDBetterR {
             // TODO: Login mechanism when using session cookie.
         }
 
-        bool APIModel::loginUsernamePassword() {;
-            auto loginPost = cpr::Post(cpr::Url{LOG_IN_FORM_URL},
-            cpr::Payload{
+        bool APIModel::loginUsernamePassword() {
+            this->session->SetUrl(LOGIN_URL);
+            this->session->SetPayload({
                 {USERNAME_FIELD, this->config.at(USERNAME_FIELD)},
                 {PASSWORD_FIELD, this->config.at(PASSWORD_FIELD)}
             });
-
-            if (loginPost.url == LOGGED_IN_URL) {
-                // TODO: Logic for a successful login.
+            auto loginPost = this->session->Post();
+            if (loginPost.url == HOME_URL) {
+                nlohmann::json accountInfo = this->request(INDEX_ACTION);
+                this->authkey = *accountInfo.find(AUTHKEY_FIELD);
+                this->passkey = *accountInfo.find(PASSKEY_FIELD);
+                this->userId = *accountInfo.find(ID_FIELD);
             } else {
                 // TODO: Logic for a failed login.
             }
+        }
+
+        nlohmann::json APIModel::request(const std::string & action) {
+            cpr::Parameters requestGetParameters;
+            requestGetParameters.AddParameter(cpr::Parameter(ACTION_PARAMETER, action));
+            if (!this->authkey.empty()) {
+                requestGetParameters.AddParameter(cpr::Parameter(AUTH_PARAMETER, this->authkey));
+            }
+            this->session->SetUrl(AJAX_URL);
+            this->session->SetParameters(requestGetParameters);
+            auto requestGet = this->session->Get();
+            nlohmann::json responseJson = nlohmann::json::parse(requestGet.text);
+            if (*responseJson.find(STATUS_FIELD) == SUCCESS_RESPONSE) {
+                return *responseJson.find(RESPONSE_FIELD);
+            }
+            // TODO: Handle bad request.
+            return "";
         }
     }
 }
--- src/model/APIModel.h
+++ src/model/APIModel.h
@@ -1,6 +1,9 @@
 #ifndef API_MODEL
 #define API_MODEL
 
+#include <cpr/cpr.h>
+#include "../../opt/json.hpp"
+
 #include <string>
 #include <map>
 
@@ -8,8 +11,19 @@ namespace REDBetterR {
     namespace API {
         const std::string USERNAME_FIELD = "username";
         const std::string PASSWORD_FIELD = "password";
-        const std::string LOG_IN_FORM_URL = "https://redacted.ch/login.php";
-        const std::string LOGGED_IN_URL = "https://redacted.ch/index.php";
+        const std::string BASE_URL = "https://redacted.ch/";
+        const std::string LOGIN_URL = BASE_URL + "login.php";
+        const std::string HOME_URL = BASE_URL + "index.php";
+        const std::string AJAX_URL = BASE_URL + "ajax.php";
+        const std::string ACTION_PARAMETER = "action";
+        const std::string AUTH_PARAMETER = "auth";
+        const std::string INDEX_ACTION = "index";
+        const std::string STATUS_FIELD = "status";
+        const std::string SUCCESS_RESPONSE = "success";
+        const std::string RESPONSE_FIELD = "response";
+        const std::string AUTHKEY_FIELD = "authkey";
+        const std::string PASSKEY_FIELD = "passkey";
+        const std::string ID_FIELD = "id";
 
         class APIModel {
         public:
@@ -21,7 +35,12 @@ namespace REDBetterR {
             bool loginUsernamePassword();
 
         private:
+            cpr::Session* session = new cpr::Session();
             std::map<std::string, std::string> config;
+            std::string authkey;
+            std::string passkey;
+            unsigned int userId;
+            nlohmann::json request(const std::string & action);
         };
     }
 }
